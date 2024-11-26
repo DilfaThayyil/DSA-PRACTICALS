066b30037c6a34079022e491c1c61bf5eb9d1ac9
DilfaThayyil <dilfathayyil@gmail.com>
2024-11-26 09:17:53 +0530

    recursion

--- DSA 1/recursion.js
+++ DSA 1/recursion.js
@@ -52,22 +52,22 @@
     // // mutual recursion
     // //*******************
 
-// function isEven(n){
-//     if(n===0){
-//         return true
-//     }
-//     return isOdd(n-1)
-// }
+function isEven(n){
+    if(n===0){
+        return true
+    }
+    return isOdd(n-1)
+}
 
-// function isOdd(n){
-//     if(n===0){
-//         return false
-//     }
-//     return isEven(n-1)
-// }
+function isOdd(n){
+    if(n===0){
+        return false
+    }
+    return isEven(n-1)
+}
 
-// console.log(isEven(4));
-// console.log(isOdd(3));
+console.log(isEven(4));
+console.log(isOdd(3));
 
 
     // binarysearch using recursion
